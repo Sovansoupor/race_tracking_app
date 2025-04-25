9a4cb548e30eb6fde01f69150aa7bb5eceeea4b3
Sovansoupor <sovansouporeng@gmail.com>
2025-04-25 17:28:16 +0700

    RTA-27 [FrontEnd] Implement Bottom Navigation Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:race_tracking_app/widgets/navigation/bottom_nav_bar.dart';
 
 void main() {
   // final ParticipantRepository participantRepository = FirebaseParticipantRepository();
-  
-  runApp(const MaterialApp());
+//runApp(MaterialApp(home: RaceForm(), debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: BottomNavBar(), debugShowCheckedModeBanner: false,));
 }
-
---
+++ lib/widgets/navigation/bottom_nav_bar.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import '../../screens/home/home_screen.dart';
+import '../../screens/result/result_screen.dart';
+import 'package:race_tracking_app/theme/theme.dart';
+import '../../screens/profile/profile_screen.dart';
+
+class BottomNavBar extends StatefulWidget {
+  final int initialIndex;
+  const BottomNavBar({super.key, this.initialIndex = 0});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  late int _selectedIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.initialIndex;
+  }
+
+  void _onItemTapped(int index) {
+    if (index != _selectedIndex) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final widgetOptions = const [
+      HomeScreen(username: '', competitions: [],),
+      ResultScreen(),
+      ProfileScreen(),
+    ];
+
+    return Scaffold(
+      body: Container(
+        color: RaceColors.backgroundAccent,
+        child: IndexedStack(
+          index: _selectedIndex,
+          children: widgetOptions,
+        ),
+      ),
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: RaceColors.backgroundAccent,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: const Offset(0, -2),
+            ),
+          ],
+        ),
+        child: BottomNavigationBar(
+          items: [
+          _buildNavItem('assets/icon/home_icon.png', 'Home', 0),
+           _buildNavItem('assets/icon/result_icon.png', 'Result', 1),
+           _buildNavItem('assets/icon/time_icon.png', 'Time', 2),
+          ],
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: RaceColors.backgroundAccent,
+          selectedItemColor: RaceColors.primary,
+          unselectedItemColor: RaceColors.white,
+          showUnselectedLabels: true,
+          elevation: 0,
+          selectedLabelStyle: RaceTextStyles.label.copyWith(
+            color: RaceColors.primary,
+          ),
+          unselectedLabelStyle: RaceTextStyles.label.copyWith(
+            color: RaceColors.white,
+          ),
+          enableFeedback: false,
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem _buildNavItem(String assetPath, String label, int index) {
+    final isSelected = _selectedIndex == index;
+
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+         assetPath,
+      width: 28,
+      height: 28,
+        color: RaceColors.primary,
+      ),
+    
+      label: isSelected ? label : '',
+    );
+  }
+}
